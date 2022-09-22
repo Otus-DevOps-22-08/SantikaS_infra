0119c2144e75c8aa335619e98c3b9cf59d56564b
Alexander <santika@yandex.ru>
2022-09-22 19:43:10 +0000

    DZ N4: Fix install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 sudo apt-get update
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add --
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc >> mongo
+sudo apt-key add mongo
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" >> /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
 sudo systemctl status mongod
-#
