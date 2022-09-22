a961ce8b54c873e7ab720566e84169f9e58cee9f
Alexander <santika@yandex.ru>
2022-09-22 19:52:52 +0000

    DZ N4: Fix install_mongodb.sh Add apt-transport-https ca-certificates

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 sudo apt-get update
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc >> mongo
-sudo apt-key add mongo
+sudo apt-get install apt-transport-https ca-certificates
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc >> mongo.asc
+sudo apt-key add mongo.asc
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" >> /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
