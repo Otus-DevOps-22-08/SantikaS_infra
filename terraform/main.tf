edc8b2e9f57cd9b2e4a81e0dd72159eb4102c36b
Alexander <santika@yandex.ru>
2022-10-19 11:26:57 +0000

    DZ N6: Fix2 main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex"
-    }
-  }
-  required_version = ">= 0.13"
-}
+#terraform {
+#  required_providers {
+#    yandex = {
+#      source = "yandex-cloud/yandex"
+#    }
+#  }
+#  required_version = ">= 0.13"
+#}
 
 provider "yandex" {
   zone      = var.zone
