11ed4015ff1d56d473a4863507cf963e95f99381
Alexander <santika@yandex.ru>
2022-11-03 13:32:44 +0000

    DZ N8: Fix2 inventory.sh

--- ansible/inventory.sh
+++ ansible/inventory.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #yc compute instances list --format yaml | grep -A1 "created_at" | grep "name" | sed 's/name: //g' | sed 's/^[ ]*//g' > hostsnames.txt
 #yc compute instances list --format yaml | grep -A1 "one_to_one_nat" | grep "address" | sed 's/address: //g' | sed 's/^[ ]*//g' > hosts.txt
-hostsnames=($(yc compute instances list --format yaml | grep -A1 "created_at" | grep "name" | sed 's/name: //g' | sed 's/^[ ]*//g'))
-hosts=($(yc compute instances list --format yaml | grep -A1 "one_to_one_nat" | grep "address" | sed 's/address: //g' | sed 's/^[ ]*//g'))
+#hostsnames=($(yc compute instances list --format yaml | grep -A1 "created_at" | grep "name" | sed 's/name: //g' | sed 's/^[ ]*//g'))
+#hosts=($(yc compute instances list --format yaml | grep -A1 "one_to_one_nat" | grep "address" | sed 's/address: //g' | sed 's/^[ ]*//g'))
 if [ "$1" == "--list" ]; then
 cat<<EOF
 {
